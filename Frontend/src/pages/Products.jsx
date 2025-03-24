d23c3c6413767340d845c71b24e64f9571f59486
WhiteShadow <aayushkuinkel41n1@gmail.com>
2025-03-24 10:48:23 +0545

    margin added

--- Frontend/src/pages/Products.jsx
+++ Frontend/src/pages/Products.jsx
@@ -31,10 +31,9 @@ const Products = () => {
         <div className='row'>
 
           <div className='col-md-3'>
-            
-            <div className='my-3 d-flex justify-content-evenly shadow-lg'>
+            <div className='my-3 d-flex justify-content-evenly'>
               <input type="text" name="" id="" placeholder='Search our products' className='form-control'/>
-              <button className='btn btn-success' 
+              <button className='btn btn-success ms-2' 
               onClick={()=>searchdatas}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
             </svg></button>
